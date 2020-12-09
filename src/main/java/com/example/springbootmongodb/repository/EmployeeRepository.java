bf2af2e5cfb6079538a9c0510658816bc8225dd0
Murphy Hsieh <murphyhsieh@leandev.io>
2020-12-09 10:51:03 +0800

    connect to the local mongo server

---
+++ src/main/java/com/example/springbootmongodb/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.springbootmongodb.repository;
+
+import com.example.springbootmongodb.model.Employee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends MongoRepository<Employee, Long>{
+
+}
